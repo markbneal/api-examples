c793d2b6349fb21116c29309995bb4cd08e78b93
markbneal <33592517+markbneal@users.noreply.github.com>
2020-10-10 09:33:53 +1300

    Delete set api key in environment.R

--- set api key in environment.R
+++
@@ -1,6 +0,0 @@
-#set API key in environment 
-Sys.setenv(API_KEY = "6f5289c1db774e7ab18f38e9b7acd157")
-# apiKey <- "a5ce13271086492eb9e8cbc27f75d3fe" #secondary
-
-# for python scripts, to make API key available, use this at terminal
-#$ export API_KEY=6f5289c1db774e7ab18f38e9b7acd157
